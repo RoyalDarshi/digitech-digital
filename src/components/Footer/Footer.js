ab67eea5880e1dde189ac593b4498ca8347089c8
RoyalDarshi <priyadarshiroy92@gmail.com>
2024-10-07 12:20:12 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+// src/components/Footer/Footer.js
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>© 2024 Digitech Digital. All Rights Reserved.</p>
+            <p>Privacy Policy | Terms of Service</p>
+        </footer>
+    );
+};
+
+export default Footer;
